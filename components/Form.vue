659462b8a90a216485355e79e3213404878585da
BigBold <ant3373@yandex.ru>
2022-01-12 00:57:09 +0300

    add form

---
+++ components/Form.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <form action="" class="add_form">
+        <input type="text" placeholder="Введите наименование товара" class="add_form__input">
+        <textarea class="add_form__txtar" placeholder="Введите описание товара" name="" id="" cols="30" rows="10"></textarea>
+        <input class="add_form__input" placeholder="Введите ссылку" type="text">
+        <input class="add_form__input" placeholder="Введите цену" type="text">
+        <button class="add_form__btn">Добавить товар</button>
+    </form>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+%input-style {
+    background: #FFFEFB;
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+    border-radius: 4px;
+    border: none;
+}
+$input-padding: 10px 16px;
+$input-margin: 0 0 33px 0;
+.add_form{
+    @extend %input-style;
+    display: flex;
+    flex-direction: column;
+    padding: 24px;
+    float: left;
+    max-width: 332px;
+    .add_form__input{
+        @extend %input-style;
+        padding: $input-padding;
+        margin: $input-margin;
+    }
+    .add_form__txtar{
+        @extend %input-style;
+        padding: $input-padding;
+        margin: $input-margin;
+    }
+    .add_form__btn{
+        @extend %input-style;
+        padding: $input-padding;
+        margin: $input-margin;
+    }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <Form/>
+</div>
+</template>
+
+<script>
+import Form from "@/components/Form"
+
+export default {
+  name: 'IndexPage',
+  components: {
+    Form: Form
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  div {
+    padding: 32px;
+  }
+</style>
