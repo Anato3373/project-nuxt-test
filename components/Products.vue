2fa91dfaea94ebca22d285a809ac1be4c8ea59f8
BigBold <ant3373@yandex.ru>
2022-01-21 09:23:14 +0300

    remove prod

--- components/Products.vue
+++ components/Products.vue
@@ -2,26 +2,38 @@
   <div class="product">
     <section class="product-section" v-for="product in getProducts" :key="product.id">
       <div class="product-card">
-        <img :src="product.img" alt="">
+        <button class="del" @click="removeProduct(product.id)"><img src="../static/delete 1.png" alt=""></button>
+        <img :src="product.img" class="product-img" alt="">
+        <div class="title-box">
         <p class="title">{{ product.name }}</p>
         <p class="txt">{{ product.textarea }}</p>
-        <span class="price">{{ product.price }}</span>
+        <span class="price">{{ product.price }}<span> руб.</span></span>
+        </div>
       </div>
     </section>
   </div>
 </template>
 
 <script>
+
 export default {
   computed: {
     getProducts() {
       return this.$store.getters['products']
-    }
+    },
   },
+  methods: {
+    removeProduct(id){
+      this.$store.dispatch('removeProduct', id)
+    }
+  }
 };
+
 </script>
 
 <style lang="scss" scoped>
+$margin: 0;
+
 .product {
   height: 100%;
   width: 1400px;
@@ -29,9 +41,7 @@ export default {
   flex-wrap: wrap;
 }
 .product-section{
-
   margin: 0 16px 16px 0;
-
 }
 .product-card{
   width: 332px;
@@ -39,5 +49,56 @@ export default {
   background: #FFFEFB;
   box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04), 0px 6px 10px rgba(0, 0, 0, 0.02);
   border-radius: 4px;
+  font-family: Source Sans Pro;
+  color: #3F3F3F;
+  cursor: pointer;
+  position: relative;
+  .del{
+    display: none;
+    width: 32px;
+    height: 32px;
+    background: #FF8484;
+    border: none;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+    position: absolute;
+    top: -8px;
+    right: -8px;
+    cursor: pointer;
+  }
+  .product-img{
+    border-radius: 4px 4px 0px 0px;
+    width: 100%;
+    height: 200px;
+  }
 }
+.product-card:hover{
+  .del{
+    display: block;
+  }
+}
+.title-box{
+  box-sizing: border-box;
+  height: 220px;
+  display: flex;
+  padding: 16px 16px 24px 16px;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .title{
+    font-weight: 600;
+    font-size: 20px;
+    margin: $margin;
+  }
+  .txt{
+    margin: $margin;
+    font-size: 16px;
+  }
+  .price{
+    margin: $margin;
+    font-weight: 600;
+    font-size: 24px;
+  }
+}
+
 </style>
--- store/index.js
+++ store/index.js
@@ -12,7 +12,11 @@ export const mutations = {
         ...data[key]
       }
     })
-  }
+    state.products.reverse()
+  },
+  REMOVE_PRODUCTS(state, arr) {
+    state.products = arr
+  },
 }
 
 export const actions = {
@@ -20,14 +24,20 @@ export const actions = {
     setTimeout(async () => {
       try {
         const {data} = await axios.get('https://test-task-23b17-default-rtdb.firebaseio.com/api.json')
-        ctx.commit('ADD_TO_PRODUCTS', {data})
+          ctx.commit('ADD_TO_PRODUCTS', {data})
       } catch (error) {
         console.log(error)
       }
     }, 0)
   },
+  removeProduct(ctx, id){
+    axios.delete(`https://test-task-23b17-default-rtdb.firebaseio.com/api/${id}.json`)
+      .then(res => {
+        ctx.commit('REMOVE_PRODUCTS', this.state.products.filter(prod => prod.id !== id))
+      })
+  }
 }
 
 export const getters = {
-  products: state => state.products
+  products: state => state.products,
 }
